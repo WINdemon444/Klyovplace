cc800eedef50cbd3c823ae84b655e9d8987cde25
WINdemon444 <windemon444@gmail.com>
2026-04-11 10:31:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Налаштування для читання даних з форм
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Вказуємо папку зі статичними файлами (CSS, картинки)
+app.use(express.static(__dirname + "/public"));
+
+// Налаштовуємо EJS як шаблонізатор
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// Головна сторінка
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер KlyovPlace запущено на http://localhost:${PORT}`);
+});
