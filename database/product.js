d07cf0b0a1fe1eb1d4f61d7896efb3554c4bde18
WINdemon444 <windemon444@gmail.com>
2026-04-25 10:34:24 +0300

    added styles,more products,and header

--- database/product.js
+++ database/product.js
@@ -1,12 +1,64 @@
-const products=["rods","lures","reel combos","hooks","tackle box","fishing line","sinkers","line cutters","first aid","bobbers"]
+const products = [
+  "rods",
+  "lures",
+  "reel combos",
+  "hooks",
+  "tackle box",
+  "fishing line",
+  "sinkers",
+  "line cutters",
+  "first aid",
+  "bobbers",
+];
 const productsList = [
-    {
-        id: "rod",
-        name: "ultralight",
-        category: "Fishing Rods",
-        price: 1850,
-        description: "The Catana EX is a highly versatile spinning rod. It has a moderate-fast action, which means it is great for casting a wide variety of lures. Perfect for catching pike and perch in local rivers.",
-        image: "https://i.ebayimg.com/images/g/ny0AAeSwUW1o3ofY/s-l1200.webp" 
-    }
-]
-module.exports=productsList
+  {
+    id: "rod",
+    name: "ultralight",
+    category: "Fishing Rods",
+    price: 1850,
+    description:
+      "The Catana EX is a highly versatile spinning rod. It has a moderate-fast action, which means it is great for casting a wide variety of lures. Perfect for catching pike and perch in local rivers.",
+    image: "https://i.ebayimg.com/images/g/ny0AAeSwUW1o3ofY/s-l1200.webp",
+  },
+  {
+    id: "box of lures",
+    name: "upstream box of lures",
+    category: "lures",
+    price: 600,
+    description: "The box is filled with random lures from our workshop",
+    image: "https://duohook.ie/cdn/shop/files/sga049_800x.png?v=1745671815",
+  },
+  {
+    id: "reel cobra",
+    name: "cobra",
+    category: "Fishing reels",
+    price: 1000,
+    description: "The reel is very useful for spinnings",
+    image: "https://www.quantumfishing.com/contentassets/a93d915603aa46d29e8bde679c458eb8/quantum_benchmark_reel.png",
+  },
+  {
+    id: "bag of hooks",
+    name: "upstream bag of hooks",
+    category: "Fishing equipment",
+    price: 185,
+    description: "The bag is filled with different sized hooks",
+    image: "https://castcray.com/wp-content/uploads/2022/08/EWG-40-324x324.png",
+  },
+  {
+    id: "fishing line",
+    name: "5 meter fishing line",
+    category: "Fishing equipment",
+    price: 18.5,
+    description: "The line is very long and handy",
+    image: "https://sc04.alicdn.com/kf/H1b0be6fa2c624e93a432d127cee5e4acj.png",
+  },
+  {
+    id: "sinkers",
+    name: "upstream sinkers",
+    category: "Fishing equipment",
+    price: 200,
+    description: "The sinkers are different weight",
+    image: "https://charkbait.com/cdn/shop/files/valley-hill-tg-dropshot-sinkers-tungsten.webp?v=1713914691&width=1214",
+  },
+];
+module.exports = productsList;
