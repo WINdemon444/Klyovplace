6f7a22e40e665edbafe7b0a78256fa4474b4c13a
WINdemon444 <windemon444@gmail.com>
2026-04-25 10:44:46 +0300

    added styles and single product page

--- app.js
+++ app.js
@@ -22,6 +22,25 @@ app.get("/", (req, res) => {
 app.get("/catalog", (req, res) => {
   res.render("catalog.ejs", { items: productsList });
 });
+app.get("/product/:id", (req, res) => {
+  // 1. Отримуємо ID товару з посилання
+  const requestedId = req.params.id;
+
+  // 2. Шукаємо цей товар у нашій базі даних
+  const foundProduct = productsList.find((item) => item.id === requestedId);
+
+  // 3. Якщо товар знайдено - показуємо його сторінку
+  if (foundProduct) {
+    res.render("product.ejs", { product: foundProduct });
+  } else {
+    // Якщо такого ID не існує (наприклад, хтось ввів /product/123)
+    res
+      .status(404)
+      .send(
+        "<h1>Товар не знайдено 😢</h1><a href='/catalog'>Повернутися в каталог</a>",
+      );
+  }
+});
 app.listen(PORT, () => {
   console.log(`Сервер KlyovPlace запущено на http://localhost:${PORT}`);
 });
